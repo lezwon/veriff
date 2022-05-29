f48a94f5662ab9b752f34e05b5055f5d0f856413
Lezwon Castelino <lezwon@gmail.com>
2022-05-29 18:25:30 +0530

    refactored code

--- classifier.py
+++ classifier.py
@@ -5,7 +5,7 @@ import time
 import cv2
 import httpx
 import numpy as np
-import tensorflow.compat.v2 as tf
+import tensorflow as tf
 import tensorflow_hub as hub
 
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"  # Disable Tensorflow logging
@@ -27,16 +27,18 @@ class BirdClassifier:
     Classifier class for Birds
     """
 
-    @staticmethod
-    def load_model():
+    def __init__(self, model_url, labels_url):
         """
-        Loads the model using Keras
-        :return:
+        Initialize the classifier
         """
         # TODO: Put in GPU
-        return hub.KerasLayer(model_url)
+        self.bird_model = hub.KerasLayer(model_url)
+        self.bird_labels = self.load_and_cleanup_labels(labels_url)
+        self.client = httpx.AsyncClient()
 
-    def load_and_cleanup_labels(self):
+        self.warmup()  # Warm up the model
+
+    def load_and_cleanup_labels(self, labels_url):
         """
         Read and parse labels into a list
         :return:
@@ -75,14 +77,33 @@ class BirdClassifier:
         # Generate tensor
         return tf.convert_to_tensor(image, dtype=tf.float32)
 
-    async def main(self):
+    def _get_top_n_scores(self, model_raw_output, k=3):
         """
-        Main function to run the code
+        Get the scores and indices of the top k scores
+        :param model_raw_output:
+        :param k:
         :return:
         """
-        self.bird_model = self.load_model()
-        self.bird_labels = self.load_and_cleanup_labels()
-        self.client = httpx.AsyncClient()
+        return tf.math.top_k(model_raw_output, k=k)
+
+    def warmup(self):
+        """
+        Warm Up the model with some sample tensors.
+        :return:
+        """
+
+        # Create a random float tensor of size (4, 224, 244)
+        image_tensor = tf.random.uniform((4, 224, 224, 3))
+        # Call the model on the tensor in a loop 5 times
+        for _ in range(5):
+            self.bird_model.call(image_tensor)
+
+    async def infer(self, image_urls):
+        """
+        Infers the given image_urls.
+        :param image_urls: List of image urls
+        :return:
+        """
 
         image_list = await asyncio.gather(
             *[self.preprocess(url) for i, url in enumerate(image_urls)]
@@ -93,36 +114,37 @@ class BirdClassifier:
         model_raw_output = self.bird_model.call(image_tensor)
 
         # TODO: Print results to kubernetes log
-        scores, indices = self.get_top_n_scores(model_raw_output)
+        self.post_process(model_raw_output)
 
+    def post_process(self, model_raw_output):
+        """
+        Post process the model output.
+        :param model_raw_output:
+        :return:
+        """
+        scores, indices = self._get_top_n_scores(model_raw_output)
         for image_index, (im_scores, im_indices) in enumerate(
             zip(scores.numpy(), indices.numpy()), start=1
         ):
             print(f"Run: {image_index}")
 
             print(
-                f'Top match: "{self.bird_labels[im_indices[0]]}" with score: {im_scores[0]}'
+                f'Top match: "{self.bird_labels[im_indices[0]]}"'
+                f" with score: {im_scores[0]}"
             )
             print(
-                f'Second match: "{self.bird_labels[im_indices[1]]}" with score: {im_scores[1]}'
+                f'Second match: "{self.bird_labels[im_indices[1]]}" '
+                f"with score: {im_scores[1]}"
             )
             print(
-                f'Third match: "{self.bird_labels[im_indices[2]]}" with score: {im_scores[2]}'
+                f'Third match: "{self.bird_labels[im_indices[2]]}" '
+                f"with score: {im_scores[2]}"
             )
             print("\n")
-
-    def get_top_n_scores(self, model_raw_output, k=3):
-        """
-        Get the scores and indices of the top k scores
-        :param model_raw_output:
-        :param k:
-        :return:
-        """
-        return tf.math.top_k(model_raw_output, k=k)
 
 
 if __name__ == "__main__":
     start_time = time.time()
-    classifier = BirdClassifier()
-    asyncio.run(classifier.main())
+    classifier = BirdClassifier(model_url, labels_url)
+    asyncio.run(classifier.infer(image_urls))
     print("Time spent: %s" % (time.time() - start_time))
